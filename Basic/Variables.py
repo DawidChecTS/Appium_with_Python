02251157e2566248d671fcbfbf20bc36baa45c9c
Your Name <checdd@gmail.com>
2024-05-31 14:00:06 +0200

    Commit 1

---
+++ Basic/Variables.py
@@ -0,0 +1,13 @@
+"""
+Variables are containers for storing values.
+We can store any data values in python variable
+In python no need to declare any command to create a variable.
+A variable is created the moment you first assign a value to it.
+
+Python variables Rules
+=========================
+A variable name must start with a letter or the underscore character (a = 5 or _name= "python" or name = "python" )
+A variable name cannot start with a number (a_a = 5 or 1name ="python" )
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _) (name_1 = "python)
+Variable names are case-sensitive (name, Name and NAME are three different variables)
+"""
