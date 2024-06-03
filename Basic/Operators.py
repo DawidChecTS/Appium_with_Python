0f1326abff8ebd8285a2f3fadb923b265528353d
Dawid_Chec <dawid.chec@testscouts.se>
2024-06-03 09:37:16 +0200

    Adding Operators file

---
+++ Basic/Operators.py
@@ -0,0 +1,45 @@
+"""
+To perform the operations on variables and itd values we will use Operators.
+
+In Python there are divides the operators into 7 groups.
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+"""
+
+# ********************************* 1. Arithmetic Operators ******************************************
+"""
+Arithmetic Operators as Addition(+), Subtraction (-), Multiplication(*),
+Division(/), Floor division (//), Exponentation(**) and Modulus (%).
+"""
+
+a = 10
+b = 5
+print(a +b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a ** b)
+print(a % b)
+
+"""
+Comparison Operators
+Equals: a == b
+Not Equals: a!=b
+Less then: a < b
+Less then or equal too: a <= b
+Greater then a > b
+Greater than or equal to: a >= b
+
+Logical Operators: and, or
+
+Identity Operators such as: is, is not
+
+Membership Operators such as: in, not in 
+"""
