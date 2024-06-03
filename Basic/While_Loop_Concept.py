c4b50f45e2a57a04c70834481ee42bea173a02a8
Dawid_Chec <dawid.chec@testscouts.se>
2024-06-03 15:11:47 +0200

    Adding While_Loop_Concept file

---
+++ Basic/While_Loop_Concept.py
@@ -0,0 +1,33 @@
+"""
+While loop will execute the statements until condition is true.
+
+initialize the value
+while condition:
+    increment the value
+"""
+
+#a = 0
+
+#while a <= 10:
+#    print(a)
+#    a += 1
+
+#b = 0
+#while a < 10:
+#    print(b)
+#    if b == 5:
+#        break
+#    b += 1
+
+#while a < 10:
+#    a += 1
+#    if a == 4:
+#        continue
+#    print(a)
+
+b = 1
+while b < 10:
+    print(b)
+    b += 1
+else:
+    print("B is no longer less then 10!")
